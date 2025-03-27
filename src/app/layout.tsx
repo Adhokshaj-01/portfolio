b236f2369bb324761fe6ae261398461304498a06
Adhokshaj <adhokshajhartalkar25@gmail.com>
2025-03-27 23:56:56 +0530

    Added the g-tracker removed cokkie consent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,9 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { SplashScreenProvider } from "@/context/SplashScreenContext";
-import CookieConsent from "@/components/cookie/cookie";
-import GoogleAnalytics from "@/components/cookie/GoogleA";
+import Script from "next/script";
+// import CookieConsent from "@/components/cookie/cookie";
+// import GoogleAnalytics from "@/components/cookie/GoogleA";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -63,13 +64,24 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
           fontSans.variable
         )}
-      > <SplashScreenProvider>
+      > 
+    {/* Google Analytics Scripts */}
+    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-SPHPFM7S40`} />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-SPHPFM7S40');
+        `}
+      </Script>
+      <SplashScreenProvider>
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
             {children}
             <Navbar />
-            <GoogleAnalytics/>
-            <CookieConsent/>
+            {/* <GoogleAnalytics/> */}
+            {/* <CookieConsent/> */}
           </TooltipProvider>
         </ThemeProvider>
         </SplashScreenProvider>
